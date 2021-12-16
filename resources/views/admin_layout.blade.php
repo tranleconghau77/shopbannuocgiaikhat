33e5ac58552bd6814a6dd4b2333df442fdcf6a3a
tranleconghau77 <tranleconghau772001@gmail.com>
2021-12-16 15:40:47 +0700

    update backend folder

---
+++ resources/views/admin_layout.blade.php
@@ -0,0 +1,155 @@
+<!--A Design by W3layouts
+Author: W3layout
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE html>
+
+<head>
+    <title>Admin</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="keywords" content="Admin" />
+    <script type="application/x-javascript">
+    addEventListener("load", function() {
+        setTimeout(hideURLbar, 0);
+    }, false);
+
+    function hideURLbar() {
+        window.scrollTo(0, 1);
+    }
+    </script>
+    <!-- bootstrap-css -->
+    <link href="{{asset('backend/css/bootstrap.min.css')}}" rel="stylesheet" />
+
+    <!-- Custom CSS -->
+    <link href="{{asset('backend/css/style.css')}}" rel='stylesheet' type='text/css' />
+    <link href="{{asset('backend/css/style-responsive.css')}}" rel="stylesheet" />
+
+    <!-- font CSS -->
+    <link
+        href='//fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic,900,900italic'
+        rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="{{asset('backend/css/font.css')}}" type="text/css" />
+
+    <!-- font-awesome icons -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <link rel="stylesheet" href="{{asset('backend/css/morris.css')}}" type="text/css" />
+
+    <!-- calendar -->
+    <link rel="stylesheet" href="{{asset('backend/css/monthly.css')}}">
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"
+        integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+    <script src="{{asset('backend/js/monthly.js')}}"></script>
+
+    <script src="{{asset('backend/js/raphael-min.js')}}">
+    </script>
+    <script src="{{asset('backend/js/morris.js')}}">
+    </script>
+</head>
+
+<body>
+    <section id="container">
+        <!--header start-->
+        <header class="header fixed-top clearfix">
+            <!--logo start-->
+            <div class="brand">
+                <a href="index.html" class="logo">
+                    ADMIN
+                </a>
+                <div class="sidebar-toggle-box">
+                    <div class="fa fa-bars"></div>
+                </div>
+            </div>
+            <!--logo end-->
+            <div class="nav notify-row" id="top_menu">
+
+            </div>
+            <div class="top-nav clearfix">
+                <!--search & user info start-->
+                <ul class="nav pull-right top-menu">
+                    <li>
+                        <input type="text" class="form-control search" placeholder=" Search">
+                    </li>
+                    <!-- user login dropdown start-->
+                    <li class="dropdown">
+                        <a data-toggle="dropdown" class="dropdown-toggle" href="#">
+                            <img alt="" src="{{asset('backend/images/1.png')}}">
+                            <span class="username">John Doe</span>
+                            <b class="caret"></b>
+                        </a>
+                        <ul class="dropdown-menu extended logout">
+                            <li><a href="#"><i class=" fa fa-suitcase"></i>Profile</a></li>
+                            <li><a href="#"><i class="fa fa-cog"></i> Settings</a></li>
+                            <li><a href="login.html"><i class="fa fa-key"></i> Log Out</a></li>
+                        </ul>
+                    </li>
+                    <!-- user login dropdown end -->
+
+                </ul>
+                <!--search & user info end-->
+            </div>
+        </header>
+        <!--header end-->
+        <!--sidebar start-->
+        <aside>
+            <div id="sidebar" class="nav-collapse">
+                <!-- sidebar menu start-->
+                <div class="leftside-navigation">
+                    <ul class="sidebar-menu" id="nav-accordion">
+                        <li>
+                            <a class="active" href="{{URL::to('/dashboard')}}">
+                                <i class="fa fa-dashboard"></i>
+                                <span>Dashboard</span>
+                            </a>
+                        </li>
+
+                        <li class="sub-menu">
+                            <a href="javascript:;">
+                                <i class="fa fa-book"></i>
+                                <span>Products</span>
+                            </a>
+                            <ul class="sub">
+                                <li><a href="typography.html">Add Products</a></li>
+                                <li><a href="glyphicon.html">Report</a></li>
+
+                            </ul>
+
+                    </ul>
+                </div>
+                <!-- sidebar menu end-->
+            </div>
+        </aside>
+        <!--sidebar end-->
+        <!--main content start-->
+        <section id="main-content">
+            <section class="wrapper">
+                @yield('admin_content')
+            </section>
+            <!-- footer -->
+            <div class="footer">
+                <div class="wthree-copyright">
+                    <p>© 2022 Visitors. All rights reserved | Design by <a href="http://w3layouts.com">W3layouts</a></p>
+                </div>
+            </div>
+            <!-- / footer -->
+        </section>
+        <!--main content end-->
+    </section>
+    <script src="{{asset('backend/js/bootstrap.js')}}"></script>
+    <script src="{{asset('backend/js/jquery.dcjqaccordion.2.7.js')}}"></script>
+    <script src="{{asset('backend/js/scripts.js')}}"></script>
+    <script src="{{asset('backend/js/jquery.slimscroll.js')}}"></script>
+    <script src="{{asset('backend/js/jquery.nicescroll.js')}}"></script>
+    <!--[if lte IE 8]><script language="javascript" type="text/javascript" src="js/flot-chart/excanvas.min.js"></script><![endif]-->
+    <script src="{{asset('backend/js/jquery.scrollTo.js')}}"></script>
+    <!-- morris JavaScript -->
+
+
+</body>
+
+</html>
