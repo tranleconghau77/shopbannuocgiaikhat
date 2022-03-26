4c2862158f29e0cdf61f7bbd0e105bbd71e2c458
tranleconghau77 <tranleconghau772001@gmail.com>
2022-03-26 22:44:41 +0700

    mmm

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,7 @@ use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
+    
     /**
      * Register any application services.
      *
@@ -23,6 +24,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if (env('APP_ENV') != 'local') {
+        URL::forceScheme('https');
+    } 
     }
 }
--- database/migrations/2022_03_26_144547_tbl_order.php
+++ database/migrations/2022_03_26_144547_tbl_order.php
@@ -16,9 +16,9 @@ class TblOrder extends Migration
         //
         Schema::create('tbl_order', function (Blueprint $table) {
             $table->id('order_id');
-            $table->integer('customer_id');
-            $table->integer('shipping_id');
-            $table->integer('payment_id');
+            $table->foreign('customer_id')->references('customer_id')->on('tbl_customers');
+            $table->foreign('shipping_id')->references('shipping_id')->on('tbl_shipping');
+            $table->foreign('payment_id')->references('payment_id')->on('tbl_payment');
             $table->double('order_total',8,2);
             $table->string('order_status');
             $table->timestamps();
