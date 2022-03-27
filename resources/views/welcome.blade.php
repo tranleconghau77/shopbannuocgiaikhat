c7a7957e780a93f796302e6721746be9e7f96466
tranleconghau77 <tranleconghau772001@gmail.com>
2022-03-27 07:41:49 +0700

    sdgsadfg

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,9 +10,9 @@
     <title>Home | E-Shopper</title>
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-    <link href="{{url('frontend/css/bootstrap.min.css')}}" rel="stylesheet" />
+    <link href="/assets/public/frontend/css/bootstrap.min.css" rel="stylesheet" />
     <link href="{{asset('frontend/css/word_animation.css')}}" rel="stylesheet" />
-    <link href="frontend/css/card_animation.css" rel="stylesheet" />
+    <link href="public/frontend/css/card_animation.css" rel="stylesheet" />
     <link href="{{secure_asset('frontend/css/fontawesome.min.css')}}" rel="stylesheet" />
     <link href="{{secure_asset('frontend/css/prettyPhoto.css')}}" rel="stylesheet" />
     <link href="{{secure_asset('frontend/css/price-range.css')}}" rel="stylesheet" />
